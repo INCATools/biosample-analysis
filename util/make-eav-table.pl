be870a1033eb376c2f645e55995749447e1dfc66
Bill Duncan <wdduncan@gmail.com>
2020-08-13 13:15:04 -0400

    add perl script for outputting an entity-attribute-value table from the biosample xml data

---
+++ util/make-eav-table.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+$writeHeader = 1;
+
+while(<>) {
+		if ($writeHeader) {
+				print "biosample\tattribute\tvalue\n";
+				$writeHeader = 0;
+		}
+		
+		if (m@<Id .*is_primary="1">(.*)</Id>@) {
+				$id = $1;
+		}
+
+		if (m@.*harmonized_name="(\S+)".*">(.*)</Attribute>@) {
+				print "$id\t$1\t$2\n";
+		}
+}
+
+
