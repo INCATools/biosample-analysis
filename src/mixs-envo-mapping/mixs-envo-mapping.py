1272b501307b0eef9ee793389b7c6852d831b57f
Mark Andrew Miller <MAM@lbl.gov>
2021-03-31 10:45:22 -0400

    added basic click CLI

--- src/mixs-envo-mapping/mixs-envo-mapping.py
+++ src/mixs-envo-mapping/mixs-envo-mapping.py
@@ -10,19 +10,15 @@ import tempfile
 import urllib.request
 from ontobio.ontol_factory import OntologyFactory
 from tdda import rexpy
+import click
 
-sql_file = "/home/mam/harmonized_table.db"
-expected_total_rows = 14000000
-# envo_url = 'https://raw.githubusercontent.com/EnvironmentOntology/envo/master/envo.owl'
-envo_json_url = 'http://purl.obolibrary.org/obo/envo.json'
-onto_prefix = "ENVO"
-
-cnx = sqlite3.connect(sql_file)
+#prepare to use a SQLite connection as a global
+cnx = None
 
 # # this count may be as slow as any subsequent query!
 # # and SQLite doesn't seem to have a fast sample random rows function
-# expected_total_rows = pds.read_sql("select count(1) from biosample", cnx)
-# expected_total_rows = expected_total_rows.iloc[0][0]
+# dbrows = pds.read_sql("select count(1) from biosample", cnx)
+# dbrows = dbrows.iloc[0][0]
 
 def json_url_to_graph(onto_url):
     fp = tempfile.NamedTemporaryFile(suffix='.json')
@@ -47,25 +43,29 @@ def discover_id_pattern(example_ids):
     extracted = re.sub("\$$", "", extracted)
     return extracted
     
-
-def get_and_tabulate(colname, expected_total_rows = 1000, row_fraction = 1.0):
-    keep_sql_num = expected_total_rows
-    if row_fraction < 1.0:
-        keep_sql_num = int(expected_total_rows * row_fraction)
+# removed row_fraction = 1.0
+def get_and_tabulate(colname, dbrows):
+    # keep_sql_num = dbrows
+    # if row_fraction < 1.0:
+    #     keep_sql_num = int(dbrows * row_fraction)
 
     pre_query = datetime.now()
-    print("Reading " + str(keep_sql_num) + " rows (" + str(row_fraction) + "), starting at")
+    print("Reading " + str(dbrows) + " rows, starting at")
     print(pre_query)
+    print('\n')
 
     #sql = "select env_broad_scale, env_local_scale, env_medium from biosample limit " + str(keep_sql_num)
-    q = "select " + colname + " from biosample limit " + str(keep_sql_num)
+    q = "select " + colname + " from biosample limit " + str(dbrows)
     df = pds.read_sql(q, cnx)
 
     post_query = datetime.now()
     print("Finished reading at")
     print(post_query)
+    print('\n')
 
+    print("SQLite read duration:")
     print(post_query - pre_query)
+    print('\n')
 
     counts_series = df[colname].value_counts()
     counts_frame = counts_series.to_frame()
@@ -76,32 +76,63 @@ def get_and_tabulate(colname, expected_total_rows = 1000, row_fraction = 1.0):
 
 def decompose_extracted(tabulated_frame, pattern):
     concat_pattern = pattern
-    compiled_pattern = re.compile(concat_pattern)
-    with_flag = tabulated_frame['string'].str.contains(compiled_pattern)
-    tabulated_frame['term_found'] = with_flag
+    #compiled_pattern = re.compile(concat_pattern)
+    #with_flag = tabulated_frame['string'].str.contains(compiled_pattern)
+    #tabulated_frame['term_found'] = with_flag
     #
     for_capture = '(' + concat_pattern + ')'
     p = re.compile(for_capture)
     extracts = tabulated_frame['string'].str.extract(p)
     tabulated_frame['found_term'] = extracts
+    tabulated_frame['found_term'] = tabulated_frame['found_term'].fillna('')
     for_replacement = '\[?' + concat_pattern + '\]?'
-    remaining_string = broad_frame['string'].replace(to_replace = for_replacement, value = '', regex = True)
+    remaining_string = tabulated_frame['string'].replace(to_replace = for_replacement, value = '', regex = True)
     residual_prefix = '^[A-Z]+:'
     if re.match(residual_prefix,pattern):
-        print("matches")
+        # print("matches")
         remaining_string = remaining_string.replace(to_replace = residual_prefix, value = '', regex = True)
     tabulated_frame['remaining_string'] = remaining_string
     return tabulated_frame
 
-graph = json_url_to_graph(envo_json_url)
-example_ids = node_ids_from_graph(graph)
-example_ids = scope_ids(example_ids, onto_prefix)
-pattern = discover_id_pattern(example_ids)
+# required and default proably incompatible/redundant
+@click.command()
+@click.option('--dbfile',
+              default="/home/mam/harmonized_table.db",
+              help='Path to SQLite requiring annotations.',
+              required=True)
+# OR get expected total rows and desired fraction
+# does that make things earier for anybody?
+@click.option('--dbrows',
+              default=1000,
+              help='Path to SQLite requiring annotations.',
+              required=True)
+@click.option('--ontourl',
+              default='http://purl.obolibrary.org/obo/envo.json',
+              help='URL of a JSON reference ontology.',
+              required=True)
+@click.option('--ontoprefix',
+              default='ENVO',
+              help='Prefix corresponding to JSON reference ontology.',
+              required=True)
+def clickmain(dbfile, dbrows, ontourl, ontoprefix):
+    # print(str(dbfile))
+    # print(str(dbrows))
+    # print(str(ontourl))
+    # print(str(ontoprefix))
+    global cnx
+    cnx = sqlite3.connect(dbfile)
+    graph = json_url_to_graph(ontourl)
+    example_ids = node_ids_from_graph(graph)
+    example_ids = scope_ids(example_ids, ontoprefix)
+    pattern = discover_id_pattern(example_ids)
+    
+    # removed row_fraction = 0.1
+    broad_frame = get_and_tabulate("env_broad_scale", dbrows)
+    broad_frame = decompose_extracted(broad_frame, pattern)
 
-broad_frame = get_and_tabulate("env_broad_scale", expected_total_rows, row_fraction = 0.1)
-broad_frame = decompose_extracted(broad_frame, pattern)
-
-print(broad_frame)
+    print(broad_frame)
+    
+clickmain()
 
 # ### what kind of IRIs do we expect from ENVO?
 # deprecated in favor of the ontobio download and parse above
